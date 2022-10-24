0b364f2c8903969e3f3d0736381bff0fe3033370
Megha <Meghashenoy87@gmail.com>
2022-10-24 10:10:14 -0500

    created ThreeCardLogic Class

---
+++ src/main/java/TheeCardLogic.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class TheeCardLogic {
+
+    // private variables
+
+
+
+    //
+    // returns an integer depending on the hand
+    //
+    //
+    // evalHand
+    //
+    public int evalHand(ArrayList<Card> hand) {
+        return 0;
+    }
+
+
+    //
+    //
+    //
+    // evalPPWinnings
+    //
+    public int evalPPWinnings(ArrayList<Card> hand, int bet) {
+        return 0;
+    }
+
+
+    //
+    //
+    //
+    // compareHands
+    //
+    public int compareHands(ArrayList<Card> dealer, ArrayList<Card> player) {
+        return 0;
+    }
+
+
+
+}
